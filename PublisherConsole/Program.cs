6fb2c6033b5d440e864c37ee9d36449c7fe46d8b
Thilakshan Baskaran <telakshanb@gmail.com>
2023-07-18 17:15:36 -0400

    add author

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+void AddAuthor()
+{
+    var author = new Author { FirstName = "Patrick", LastName = "Rothfuss" };
+
+    using var context = new PubContext();
+
+    context.Authors.Add(author);
+
+    context.SaveChanges();
+}
+
+GetAuthors();
+
+AddAuthor();
+
+GetAuthors();
+
+void GetAuthors()
+{
+    using var context  = new PubContext();
+    var authors = context.Authors.ToList();
+
+    authors.ForEach(x => Console.Write(x.FirstName + " " + x.LastName));
+}
