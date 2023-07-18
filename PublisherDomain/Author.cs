b557f095b62b238237ad7d53f4647494824a1443
Thilakshan Baskaran <telakshanb@gmail.com>
2023-07-18 17:16:23 -0400

    add author

--- PublisherDomain/Author.cs
+++ PublisherDomain/Author.cs
@@ -10,6 +10,5 @@ public class Author
     public int Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
-
     public List<Book> Books { get; set; }
 }
